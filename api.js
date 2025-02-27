b635f0ed42ac4a7d418b91504f4cfa4c8c9bb6a1
LeonidasVS <villafrancosibrian@gmail.com>
2025-02-26 20:44:21 -0600

    CRUD API FINALIZADO

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const API_URL = "https://localhost:7198";
 var editar = false;
-let botonActualizarExiste=document.getElementById("btnActualizar");
-
+let botonActualizarExiste = document.getElementById("btnActualizar");
+let validar = false;
 //Obtener todos los productos
 function ObtenerListado() {
   fetch(`${API_URL}/api/Listarproducto`)
@@ -16,8 +16,8 @@ function ObtenerListado() {
       body += `<tr><td>${data[i].idProducto}</td><td>${
         data[i].nombre
       }</td><td>${"$" + data[i].precio}</td><td>${data[i].stock}</td>
-      <td><button type="button" class="btn btn-danger btnEliminar ">Eliminar</button></td>
-      <td><a href="#"><button id="update" class="btn btn-primary btnEditar" type="button">Editar</button></a></td>
+      <td><button type="button" class="btn btn-danger btnEliminar ">D <i class="bi bi-trash3"></i></button></td>
+      <td><button id="update" class="btn btn-primary btnEditar" type="button">U <i class="bi bi-pencil-square"></i></button></td>
       </tr>
       `;
     }
@@ -33,7 +33,7 @@ function buscarProducto() {
   fetch(`${API_URL}/api/Obtenerporid/${id}`)
     .then((response) => response.json())
     .then((data) => mostrarData(data))
-    .catch((error) => alert("El ID no existe"));
+    .catch((error) => alert("El ID no existe"+error));
 
   const mostrarData = (data) => {
     console.log(data);
@@ -49,40 +49,48 @@ function buscarProducto() {
   };
 }
 
-if(botonActualizarExiste){
-  document.getElementById("btnActualizar").addEventListener('click', function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get("id");
-  
-    const nombrepro = document.getElementById("nombreProducto").value;
-    const preciopro = document.getElementById("precioProducto").value;
-    const stockpro = document.getElementById("stockProducto").value;
-  
-    fetch(`${API_URL}/api/Actualizar`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        idProducto: id,
-        nombre: nombrepro,
-        precio: preciopro,
-        stock: stockpro,
-      }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          alert("Producto actualizado");
-          ObtenerListado();
-          window.location.replace("http://127.0.0.1:5501/index.html#");
-        } else {
-          throw new Error("Error al insertar");
-        }
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  });
+if (botonActualizarExiste) {
+  document
+    .getElementById("btnActualizar")
+    .addEventListener("click", function () {
+      const urlParams = new URLSearchParams(window.location.search);
+      const id = urlParams.get("id");
+
+      const nombrepro = document.getElementById("nombreProducto").value;
+      const preciopro = document.getElementById("precioProducto").value;
+      const stockpro = document.getElementById("stockProducto").value;
+
+      validar=validarCampos(nombrepro, preciopro, stockpro);
+
+      if (validar) {
+        fetch(`${API_URL}/api/Actualizar`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            idProducto: id,
+            nombre: nombrepro,
+            precio: preciopro,
+            stock: stockpro,
+          }),
+        })
+          .then((response) => {
+            if (response.ok) {
+              alert("Producto actualizado");
+              ObtenerListado();
+              window.location.replace("http://127.0.0.1:5501/index.html#");
+            } else {
+              throw new Error("Error al insertar");
+            }
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      }else{
+        alert("Espacios en blanco");
+      }
+    });
 }
 
 //Insertar Producto
@@ -91,15 +99,9 @@ function insertarProducto() {
   const preciopro = document.getElementById("precioProducto").value;
   const stockpro = document.getElementById("stockProducto").value;
 
-  if (
-    nombrepro == "" ||
-    preciopro == "" ||
-    preciopro <= 0 ||
-    stockpro == "" ||
-    stockpro <= 0
-  ) {
-    alert("Hay campos que no cumplen");
-  } else {
+  validar = validarCampos(nombrepro, preciopro, stockpro);
+
+  if (validar) {
     fetch(`${API_URL}/api/Insertar`, {
       method: "POST",
       headers: {
@@ -123,6 +125,8 @@ function insertarProducto() {
       .catch((error) => {
         alert(error);
       });
+  } else {
+    alert("Espacios en blanco");
   }
 }
 
@@ -151,9 +155,7 @@ function validarPorID() {
   input.addEventListener("input", function () {
     if (input.value.trim() == "") {
       ObtenerListado();
-    } else {
-      console.log("Esta llleno");
-    }
+    } 
   });
 }
 
@@ -174,6 +176,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+
 document.addEventListener("DOMContentLoaded", function () {
   const urlParams = new URLSearchParams(window.location.search);
   const idProducto = urlParams.get("id");
@@ -192,6 +195,21 @@ document.addEventListener("DOMContentLoaded", function () {
       .catch((error) => console.error("Error al buscar el producto:", error));
   }
 });
+
+
+function validarCampos(nombre, precio, stock) {
+  if (
+    nombre == "" ||
+    precio == "" ||
+    precio <= 0 ||
+    stock == "" ||
+    stock <= 0
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
 ObtenerListado();
 validarPorID();
